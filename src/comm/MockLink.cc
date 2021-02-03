mavlink/qgroundcontrol

Switch COMPONENT_INFORMATION compression to lzma

src/comm/MockLink.cc
@@ -1619,7 +1619,7 @@ void MockLink::_sendVersionMetaData(void)
 {
     mavlink_message_t   responseMsg;
 #if 1
-    char                metaDataURI[MAVLINK_MSG_COMPONENT_INFORMATION_FIELD_METADATA_URI_LEN]       = "mftp://[;comp=1]version.json.gz";
+    char                metaDataURI[MAVLINK_MSG_COMPONENT_INFORMATION_FIELD_METADATA_URI_LEN]       = "mftp://[;comp=1]version.json";
 #else
     char                metaDataURI[MAVLINK_MSG_COMPONENT_INFORMATION_FIELD_METADATA_URI_LEN]       = "https://bit.ly/31nm0fs";
 #endif
@@ -1642,7 +1642,7 @@ void MockLink::_sendParameterMetaData(void)
 {
     mavlink_message_t   responseMsg;
 #if 1
-    char                metaDataURI[MAVLINK_MSG_COMPONENT_INFORMATION_FIELD_METADATA_URI_LEN]       = "mftp://[;comp=1]parameter.json";
+    char                metaDataURI[MAVLINK_MSG_COMPONENT_INFORMATION_FIELD_METADATA_URI_LEN]       = "mftp://[;comp=1]parameter.json.lzma";
 #else
     char                metaDataURI[MAVLINK_MSG_COMPONENT_INFORMATION_FIELD_METADATA_URI_LEN]       = "https://bit.ly/2ZKRIRE";
 #endif

src/comm/MockLinkFTP.cc
@@ -119,10 +119,12 @@ void MockLinkFTP::_openCommand(uint8_t senderSystemId, uint8_t senderComponentId
         tmpFilename = _createTestTempFile(sizeString.toInt());
     } else if (path == "/version.json") {
         tmpFilename = ":MockLink/Version.MetaData.json";
-    } else if (path == "/version.json.gz") {
-        tmpFilename = ":MockLink/Version.MetaData.json.gz";
+    } else if (path == "/version.json.lzma") {
+        tmpFilename = ":MockLink/Version.MetaData.json.lzma";
     } else if (path == "/parameter.json") {
         tmpFilename = ":MockLink/Parameter.MetaData.json";
+    } else if (path == "/parameter.json.lzma") {
+        tmpFilename = ":MockLink/Parameter.MetaData.json.lzma";
     }
 
     if (!tmpFilename.isEmpty()) {

AleDek/qgroundcontrol_koala

src/comm/MockLink.cc
@@ -1619,7 +1619,7 @@ void MockLink::_sendVersionMetaData(void)
 {
     mavlink_message_t   responseMsg;
 #if 1
-    char                metaDataURI[MAVLINK_MSG_COMPONENT_INFORMATION_FIELD_METADATA_URI_LEN]       = "mftp://[;comp=1]version.json.gz";
+    char                metaDataURI[MAVLINK_MSG_COMPONENT_INFORMATION_FIELD_METADATA_URI_LEN]       = "mftp://[;comp=1]version.json";
 #else
     char                metaDataURI[MAVLINK_MSG_COMPONENT_INFORMATION_FIELD_METADATA_URI_LEN]       = "https://bit.ly/31nm0fs";
 #endif
@@ -1642,7 +1642,7 @@ void MockLink::_sendParameterMetaData(void)
 {
     mavlink_message_t   responseMsg;
 #if 1
-    char                metaDataURI[MAVLINK_MSG_COMPONENT_INFORMATION_FIELD_METADATA_URI_LEN]       = "mftp://[;comp=1]parameter.json";
+    char                metaDataURI[MAVLINK_MSG_COMPONENT_INFORMATION_FIELD_METADATA_URI_LEN]       = "mftp://[;comp=1]parameter.json.xz";
 #else
     char                metaDataURI[MAVLINK_MSG_COMPONENT_INFORMATION_FIELD_METADATA_URI_LEN]       = "https://bit.ly/2ZKRIRE";
 #endif

src/comm/MockLinkFTP.cc
@@ -119,10 +119,12 @@ void MockLinkFTP::_openCommand(uint8_t senderSystemId, uint8_t senderComponentId
         tmpFilename = _createTestTempFile(sizeString.toInt());
     } else if (path == "/version.json") {
         tmpFilename = ":MockLink/Version.MetaData.json";
-    } else if (path == "/version.json.gz") {
-        tmpFilename = ":MockLink/Version.MetaData.json.gz";
+    } else if (path == "/version.json.xz") {
+        tmpFilename = ":MockLink/Version.MetaData.json.xz";
     } else if (path == "/parameter.json") {
         tmpFilename = ":MockLink/Parameter.MetaData.json";
+    } else if (path == "/parameter.json.xz") {
+        tmpFilename = ":MockLink/Parameter.MetaData.json.xz";
     }
 
     if (!tmpFilename.isEmpty()) {