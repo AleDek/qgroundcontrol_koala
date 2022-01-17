mdogancay/qgroundcontrol

actuators: increase motor spin timeout to 3s when assigning motors
So that ESC's can initialize. Note that some are faster than others and 1s
is enough in some cases.

src/Vehicle/Actuators/MotorAssignment.cc
@@ -17,7 +17,7 @@
 MotorAssignment::MotorAssignment(QObject* parent, Vehicle* vehicle, QmlObjectListModel* actuators)
         : QObject(parent), _vehicle(vehicle), _actuators(actuators)
 {
-    _spinTimer.setInterval(1000);
+    _spinTimer.setInterval(_spin_timeout_default_s);
     _spinTimer.setSingleShot(true);
     connect(&_spinTimer, &QTimer::timeout, this, &MotorAssignment::spinTimeout);
 }
@@ -141,6 +141,7 @@ void MotorAssignment::start()
     }
     _state = State::Running;
     emit activeChanged();
+    _spinTimer.setInterval(_assignMotors ? _spin_timeout_high_s : _spin_timeout_default_s);
     _spinTimer.start();
 }
 
@@ -169,6 +170,7 @@ void MotorAssignment::selectMotor(int motorIndex)
         emit activeChanged();
     } else {
         // spin the next motor after some time
+        _spinTimer.setInterval(_spin_timeout_default_s);
         _spinTimer.start();
     }
 }

src/Vehicle/Actuators/MotorAssignment.h
@@ -59,6 +59,9 @@ private slots:
     void spinTimeout();
 
 private:
+    static constexpr int _spin_timeout_default_s = 1000;
+    static constexpr int _spin_timeout_high_s = 3000; ///< wait a bit longer after assigning motors, so ESCs can initialize
+
     static void ackHandlerEntry(void* resultHandlerData, int compId, MAV_RESULT commandResult, uint8_t progress,
             Vehicle::MavCmdResultFailureCode_t failureCode);
     void ackHandler(MAV_RESULT commandResult, Vehicle::MavCmdResultFailureCode_t failureCode);

AleDek/qgroundcontrol_koala

src/Vehicle/Actuators/MotorAssignment.cc
@@ -17,7 +17,7 @@
 MotorAssignment::MotorAssignment(QObject* parent, Vehicle* vehicle, QmlObjectListModel* actuators)
         : QObject(parent), _vehicle(vehicle), _actuators(actuators)
 {
-    _spinTimer.setInterval(1000);
+    _spinTimer.setInterval(_spinTimeoutDefaultSec);
     _spinTimer.setSingleShot(true);
     connect(&_spinTimer, &QTimer::timeout, this, &MotorAssignment::spinTimeout);
 }
@@ -141,6 +141,7 @@ void MotorAssignment::start()
     }
     _state = State::Running;
     emit activeChanged();
+    _spinTimer.setInterval(_assignMotors ? _spinTimeoutHighSec : _spinTimeoutDefaultSec);
     _spinTimer.start();
 }
 
@@ -169,6 +170,7 @@ void MotorAssignment::selectMotor(int motorIndex)
         emit activeChanged();
     } else {
         // spin the next motor after some time
+        _spinTimer.setInterval(_spinTimeoutDefaultSec);
         _spinTimer.start();
     }
 }

src/Vehicle/Actuators/MotorAssignment.h
@@ -59,6 +59,9 @@ private slots:
     void spinTimeout();
 
 private:
+    static constexpr int _spinTimeoutDefaultSec = 1000;
+    static constexpr int _spinTimeoutHighSec = 3000; ///< wait a bit longer after assigning motors, so ESCs can initialize
+
     static void ackHandlerEntry(void* resultHandlerData, int compId, MAV_RESULT commandResult, uint8_t progress,
             Vehicle::MavCmdResultFailureCode_t failureCode);
     void ackHandler(MAV_RESULT commandResult, Vehicle::MavCmdResultFailureCode_t failureCode);